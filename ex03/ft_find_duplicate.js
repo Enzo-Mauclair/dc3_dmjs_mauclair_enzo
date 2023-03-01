10399e73e42decc8ac2cfde59c476056c9d8592d
Enzo Mauclair <enzo.mauclair@gmail.com>
2023-03-01 19:29:33 +0100

    ex03 fini

--- ex03/ft_find_duplicate.js
+++ ex03/ft_find_duplicate.js
@@ -1,12 +1,17 @@
 function valeursidentiques(array) {
-  for (var i = 0; i < array.length; i++) {
-    for (var j = i + 1; j < array.length; j++) {
-      if (array[i] === array[j]) {
-       return true;
-     }
-   }
- }
- return false;
- }
 
+  var unique = {};
+
+  for (var i of array) 
+  {
+    if (unique[i]) 
+    
+    {
+      return "True";
+    }
+    unique[i] = true;
+  }
+
+  return "False";
+}
 console.log(valeursidentiques([87, 16, 12, 89 ,33, 33, 64,8]));
