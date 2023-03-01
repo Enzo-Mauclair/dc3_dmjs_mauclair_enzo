7acaa31b3c7ee9cc26da7b9bcafb2d5e778d9fba
Enzo Mauclair <enzo.mauclair@gmail.com>
2023-03-01 19:27:19 +0100

    exo 3 fini

---
+++ ex03/ft_find_duplicate.js
@@ -0,0 +1,12 @@
+function valeursidentiques(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i + 1; j < array.length; j++) {
+      if (array[i] === array[j]) {
+       return true;
+     }
+   }
+ }
+ return false;
+ }
+
+console.log(valeursidentiques([87, 16, 12, 89 ,33, 33, 64,8]));
